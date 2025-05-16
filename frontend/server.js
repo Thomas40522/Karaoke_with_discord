01a00d461c08afd49a7d1056a8dc2b4ffcc71d42
Thomas40522 <73463155+Thomas40522@users.noreply.github.com>
2025-05-16 18:18:01 -0400

    added frontend ssl

---
+++ frontend/server.js
@@ -0,0 +1,25 @@
+const { createServer } = require("https");
+const { parse } = require("url");
+const next = require("next");
+const fs = require("fs");
+const path = require("path");
+
+const port = 8000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+// Load SSL cert and key
+const httpsOptions = {
+  key: fs.readFileSync(path.join(__dirname, "ssl", "../../backend/*.feiyangxie.com_private_key.key")),
+  cert: fs.readFileSync(path.join(__dirname, "ssl", "../../backend/feiyangxie.com_ssl_certificate.cer")),
+};
+
+app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, () => {
+    console.log(`✅ Server running at https://alicloud.feiyang.com:${port}`);
+  });
+});
--- frontend/src/endpoints.ts
+++ frontend/src/endpoints.ts
@@ -1,3 +1,4 @@
 // export const backend_endpoint = "http://127.0.0.1:3758"
-export const backend_endpoint_default = "http://47.254.65.42:3758"
+// export const backend_endpoint_default = "http://47.254.65.42:3758"
 // export const backend_endpoint_default = "http://172.16.9.181:3758"
+export const backend_endpoint_default = "https://alicloud.feiyangxie.com:3758"
